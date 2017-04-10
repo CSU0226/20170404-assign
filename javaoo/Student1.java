deb126f71cfae361a955e0a92eb18feb1b290d1a
CSU0226 <couh5651@naver.com>
2017-04-10 21:54:38 +0900

    Student1.java

---
+++ javaoo/Student1.java
@@ -0,0 +1,14 @@
+public class Student1 {
+	String Sta;
+	int StuNum;
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s = new Student();
+		s.Sta = "컴퓨터정보공학과";
+		s.StuNum = 20141673;
+		
+		System.out.println("학과 = " + s.Sta);
+		System.out.println("학번 = " + s.StuNum);
+	}
+}
